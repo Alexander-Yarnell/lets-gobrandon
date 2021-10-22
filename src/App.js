4c01c9632fcc1f66731b3780eca9e77d24d745ca
Alexander Yarnell <yarnell.alexander@yahoo.com>
2021-10-22 15:57:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import Profile from "./Profile.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Profile}  alt="logo" />
+        <p>Hi mom, Fuck Joe Biden </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
