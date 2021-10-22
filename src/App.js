793f8fd253026904a0c4d59f7d2416fecee1172b
Alexander Yarnell <yarnell.alexander@yahoo.com>
2021-10-22 17:59:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
-import Profile from "./Profile.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Profile}  alt="logo" />
-        <p>Hi mom, Fuck Joe Biden </p>
+        <img src={logo}  alt="logo" />
+        <p>Fuck Joe Biden </p>
       </header>
     </div>
   );
